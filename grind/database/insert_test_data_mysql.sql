ba0a4ced802cd9192352e17af9300297420cbe4f
katya450 <katja.lusma@gmail.com>
2015-10-28 15:58:06 +0200

    Tietokannanluontiscriptit korjattu

--- grind/database/insert_test_data_mysql.sql
+++ grind/database/insert_test_data_mysql.sql
@@ -1,9 +1,6 @@
--- MUISTA! console encoding latin1
-
-INSERT INTO henkilo
-	(etunimi, sukunimi)
-values
-	('Matti', 'Meikäläinen'),
-	('Mikko', 'Mallikas'),
-	('Outi', 'Opiskelija'),
-	('Tiina', 'Tikolainen');
+INSERT INTO tehtava
+	(t_id, t_kuvaus, t_lisatiedot, t_deadlinedtm, t_muistutusdtm)
+VALUES
+	('11', 'Viininmaistelu', 'Vedä 5 litraa punkkua', '2015-10-24 15:17:00','2015-11-26 20:17:00'),
+	('14', 'Tiedonhallinta ja tietokannat', 'Normalisointikaaviot', '2015-11-24 20:58:00','2015-11-22 17:00:00'),
+	('16', 'English 2', 'Esseen palautus', '2015-10-30 23:55:00','2015-10-27 12:00:00');
