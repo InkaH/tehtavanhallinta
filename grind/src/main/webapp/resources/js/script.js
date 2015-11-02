f413d9a87d55a7a46b11d44dd21ba64d87770414
micronome <mika.ropponen@outlook.com>
2015-11-02 12:05:20 +0200

    CSS quick fix

--- grind/src/main/webapp/resources/js/script.js
+++ grind/src/main/webapp/resources/js/script.js
@@ -1,6 +1,3 @@
 $(document).ready(function(){
-	$("#add").click(function(){
-		$("#add").animate({height:$("#add").get(0).scrollHeight},500);
-	});
-	/*$(window).resize(function(){$("#add").animate({height:$("#add").get(0).scrollHeight},500);});*/
+	$(window).resize(function(){$("#add").animate({height:$("#add").get(0).scrollHeight},500);});
 });
