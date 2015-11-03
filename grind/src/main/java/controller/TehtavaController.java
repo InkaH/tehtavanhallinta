a26f2e6139f8b80392a8c6cc04346c7143e7f667
micronome <mika.ropponen@outlook.com>
2015-11-03 14:20:37 +0200

    controller commented

--- grind/src/main/java/controller/TehtavaController.java
+++ grind/src/main/java/controller/TehtavaController.java
@@ -15,13 +15,13 @@ import bean.Tehtava;
 @Controller
 @RequestMapping(value = "/")
 public class TehtavaController {
-
-	private Tehtava editItem = new Tehtava();
-	private List<Tehtava> tehtavat;
-	private int editingActive = 0;
+	
+	private Tehtava editItem = new Tehtava(); // controller sustain one Tehtava object in order to pre-fill the main form
+	private List<Tehtava> tehtavat; // list of all user's tasks
+	private int editingActive = 0; // editing mode (0 = false, 1 = true)
 
 	@Inject
-	private TehtavaDao dao;
+	private TehtavaDao dao; // makes all database management methods available in controller class
 
 	public TehtavaDao getDao() {
 		return dao;
@@ -30,45 +30,49 @@ public class TehtavaController {
 	public void setDao(TehtavaDao dao) {
 		this.dao = dao;
 	}
-
+	
+	// executed every time before entering index.jsp (value= "/")
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String getView(Map<String, Object> model) {
 		tehtavat = dao.haeKaikki();
-		model.put("tehtavat", tehtavat);
-		model.put("uusiTehtava", this.editItem);
-		model.put("edit", Integer.toString(editingActive));
+		model.put("tehtavat", tehtavat); // send all tasks to UI in a variable called 'tehtavat'
+		model.put("uusiTehtava", this.editItem); // send a form pre-fill object to UI in a variable called 'uusiTehtava'
+		model.put("edit", Integer.toString(editingActive)); // send the state of editing mode to UI in a variable called 'edit'
 		return "index";
 	}
-
+	
+	// task creation/editing form calls the method on submit <form action="add"...>  -->  @RequestMapping(value="add"...)
 	@RequestMapping(value = "add", method = RequestMethod.POST)
-	public String lisaaTehtava(@ModelAttribute("uusiTehtava") Tehtava task) {
+	public String lisaaTehtava(@ModelAttribute("uusiTehtava") Tehtava task) { // get the Tehtava object from the form
 		if (!task.getKuvaus().isEmpty()) {
-			dao.lisaaTehtava(task);
+			dao.lisaaTehtava(task); // if the header of task is not empty, insert the new task into database
 		}
-		editingActive = 0;
-		editItem.nollaaTehtava();
-		return "redirect:/";
+		editingActive = 0; // set editing mode off
+		editItem.nollaaTehtava(); // clear the content of the form pre-fill object
+		return "redirect:/"; // move to getView method (value = "/")
 	}
 
+	// task deletion form calls the method on submit <form action="del"...>  -->  @RequestMapping(value="del"...)
 	@RequestMapping(value = "del", method = RequestMethod.POST)
-	public String poistaTehtava(@RequestParam String delTask) {
+	public String poistaTehtava(@RequestParam String delTask) { // get attribute delTask (task id) from the form
 		int de = Integer.parseInt(delTask);
 		if (de > 0) {
-			dao.poistaTehtava(de);
+			dao.poistaTehtava(de); // if delTask > 0 (Tehtava object exists), remove the task from database
 		}
 		editingActive = 0;
 		return "redirect:/";
 	}
 
+	// task editing form calls the method on submit
 	@RequestMapping(value = "edit", method = RequestMethod.POST)
 	public String muokkaaTehtava(@RequestParam String editTask) {
 		int ed = Integer.parseInt(editTask);
 		if (ed > 0) {
-			for (Tehtava t : tehtavat) {
+			for (Tehtava t : tehtavat) { // get the right task by id
 				if (t.getId() == ed) {
-					this.editItem = t;
-					editingActive = 1;
-					return "redirect:/";
+					this.editItem = t; // copy the editable task reference to the form pre-fill object
+					editingActive = 1; // set editing mode on
+					return "redirect:/"; // quit searching and move to getView method
 				}
 			}
 		}
