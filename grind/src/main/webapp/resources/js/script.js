c00ca809277de519c5d32a310e992fe9b133ff5b
micronome <mika.ropponen@outlook.com>
2015-11-20 18:25:30 +0200

    color fix

--- grind/src/main/webapp/resources/js/script.js
+++ grind/src/main/webapp/resources/js/script.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
+	
 	$(window).resize(function() {
-		$("#add").animate({
-			height : $("#add").get(0).scrollHeight
-		}, 500);
+		$("#add").removeClass("collapse in").addClass("collapse");
 	});
 
 	$("#datepicker").datepicker();
