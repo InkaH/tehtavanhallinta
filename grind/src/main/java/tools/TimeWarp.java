1c3f190903efad5551cd575a7bb32922a8f8e93a
micronome <mika.ropponen@outlook.com>
2015-12-08 19:10:09 +0200

    quick fix

--- grind/src/main/java/tools/TimeWarp.java
+++ grind/src/main/java/tools/TimeWarp.java
@@ -6,7 +6,7 @@ public class TimeWarp {
 	
 	public static LocalDateTime startOfNextWeek() {
 		LocalDateTime now = LocalDateTime.now();
-		int dayOfWeek = now.getDayOfWeek().getValue(); // 1 moday ... 7 sunday
+		int dayOfWeek = now.getDayOfWeek().getValue(); // 1 monday ... 7 sunday
 		LocalDateTime startOfWeek = now.minusDays((long) (dayOfWeek - 1));
 		startOfWeek = LocalDateTime.of(startOfWeek.getYear(), startOfWeek.getMonthValue(), startOfWeek.getDayOfMonth(), 0, 0, 0);
 		LocalDateTime startOfNextWeek = startOfWeek.plusWeeks(1);
@@ -16,7 +16,7 @@ public class TimeWarp {
 	
 	public static LocalDateTime endOfNextWeek() {
 		LocalDateTime now = LocalDateTime.now();
-		int dayOfWeek = now.getDayOfWeek().getValue(); // 1 moday ... 7 sunday
+		int dayOfWeek = now.getDayOfWeek().getValue(); // 1 monday ... 7 sunday
 		LocalDateTime endOfWeek = now.plusDays((long) (7 - dayOfWeek));
 		endOfWeek = LocalDateTime.of(endOfWeek.getYear(), endOfWeek.getMonthValue(), endOfWeek.getDayOfMonth(), 23, 59, 59);
 		LocalDateTime endOfNextWeek = endOfWeek.plusWeeks(1);
@@ -26,7 +26,7 @@ public class TimeWarp {
 	
 	public static LocalDateTime startOfCurrentWeek() {
 		LocalDateTime now = LocalDateTime.now();
-		int dayOfWeek = now.getDayOfWeek().getValue(); // 1 moday ... 7 sunday
+		int dayOfWeek = now.getDayOfWeek().getValue(); // 1 monday ... 7 sunday
 		LocalDateTime startOfWeek = now.minusDays((long) (dayOfWeek - 1));
 		startOfWeek = LocalDateTime.of(startOfWeek.getYear(), startOfWeek.getMonthValue(), startOfWeek.getDayOfMonth(), 0, 0, 0);
 		System.out.println(startOfWeek);
@@ -35,7 +35,7 @@ public class TimeWarp {
 	
 	public static LocalDateTime endOfCurrentWeek() {
 		LocalDateTime now = LocalDateTime.now();
-		int dayOfWeek = now.getDayOfWeek().getValue(); // 1 moday ... 7 sunday
+		int dayOfWeek = now.getDayOfWeek().getValue(); // 1 monday ... 7 sunday
 		LocalDateTime endOfWeek = now.plusDays((long) (7 - dayOfWeek));
 		endOfWeek = LocalDateTime.of(endOfWeek.getYear(), endOfWeek.getMonthValue(), endOfWeek.getDayOfMonth(), 23, 59, 59);
 		System.out.println(endOfWeek);
