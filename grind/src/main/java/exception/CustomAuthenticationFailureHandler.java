a42646e53f47d76f95141f1878ed7b63783d0f86
Inka <a1300883@myy.haaga-helia.fi>
2015-12-08 10:54:24 +0200

    auth failure handler edit

--- grind/src/main/java/exception/CustomAuthenticationFailureHandler.java
+++ grind/src/main/java/exception/CustomAuthenticationFailureHandler.java
@@ -1,44 +1,33 @@
 package exception;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.dao.DataAccessException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
 
-//Luokka autentikointiin liittyvien virheiden käsittelyyn, tulee käyttöön jos tulee (ei käytössä tällä hetkellä)
-public class CustomAuthenticationFailureHandler extends
-		SimpleUrlAuthenticationFailureHandler {
-	private DefaultRedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
-	private String defaultFailureUrl = "/error";
+@Component
+public class CustomAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
 	@Override
 	public void onAuthenticationFailure(HttpServletRequest request,
 			HttpServletResponse response, AuthenticationException exception)
-			throws IOException, ServletException {
-		this.setDefaultFailureUrl(defaultFailureUrl);
+					throws IOException, ServletException {
+		super.onAuthenticationFailure(request, response, exception);
+
 		if (exception.getClass().isAssignableFrom(
 				UsernameNotFoundException.class)) {
-			this.setDefaultFailureUrl("/login");
-			this.setUseForward(true);
-			this.saveException(request, exception);
-			this.setRedirectStrategy(redirectStrategy);
-			redirectStrategy.sendRedirect(request, response,
-					this.defaultFailureUrl);
-		} else if (exception.getClass().isAssignableFrom(
+			// TODO Set the redirect
+		}
+		else if (exception.getClass().isAssignableFrom(
 				DataAccessException.class)) {
-			this.setDefaultFailureUrl("/error");
-			this.setUseForward(true);
-			this.saveException(request, exception);
-			this.setRedirectStrategy(redirectStrategy);
-			redirectStrategy.sendRedirect(request, response,
-					this.defaultFailureUrl);
+			// TODO Set the redirect
 		}
 	}
 }
+
+
