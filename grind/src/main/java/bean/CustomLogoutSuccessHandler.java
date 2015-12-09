12212bc774642fbad013242ab95173de2643caff
InkaH <a1300883@myy.haaga-helia.fi>
2015-12-09 04:25:48 +0200

    removed unused import

--- grind/src/main/java/bean/CustomLogoutSuccessHandler.java
+++ grind/src/main/java/bean/CustomLogoutSuccessHandler.java
@@ -5,13 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 import org.springframework.stereotype.Component;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
-import java.net.URLEncoder;
 
 @Component
 public class CustomLogoutSuccessHandler extends SimpleUrlLogoutSuccessHandler {
